aa0ae8de0556598c52ce9d1344261f3a54a63391
Ari <esquirol.volador@me.com>
2022-09-07 21:35:51 +0200

    Delete src/S04T02N01PuigTorreroArianna directory

--- src/S04T02N01PuigTorreroArianna/src/main/java/cat/itacademy/barcelonactiva/Puig/Arianna/s04/t02/n01/model/repository/FruitaRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.Puig.Arianna.s04.t02.n01.model.repository;
-import java.util.List;
-import cat.itacademy.barcelonactiva.Puig.Arianna.s04.t02.n01.model.domain.Fruita;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FruitaRepository extends JpaRepository<Fruita, Integer>{
-  List<Fruita> findByNom(String nom);
-  List<Fruita> findByQuantitatQuilos(int quantitatQuilos);
-}
